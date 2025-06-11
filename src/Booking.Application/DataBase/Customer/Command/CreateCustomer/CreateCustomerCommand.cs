914052f48ecf6315bd14c2a450da60c71e7224a9
PanxhiXxL <molinaedison5@gmail.com>
2025-06-11 11:52:33 -0500

    spaceBlank

--- src/Booking.Application/DataBase/Customer/Command/CreateCustomer/CreateCustomerCommand.cs
+++ src/Booking.Application/DataBase/Customer/Command/CreateCustomer/CreateCustomerCommand.cs
@@ -17,7 +17,6 @@ namespace Booking.Application.DataBase.Customer.Command.CreateCustomer
         public async Task<CreateCustomerModel> ExecuteAsync(CreateCustomerModel model)
         {
             var customerEntity = _mapper.Map<CustomerEntity>(model);
-            //customerEntity.DocumentNumber = Guid.NewGuid().ToString(); // Generate a unique document number
             await _dataBase.Customer.AddAsync(customerEntity);
             await _dataBase.SaveAsync();
 
